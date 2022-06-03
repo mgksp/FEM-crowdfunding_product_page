894ed62a66725d6e049c89263b933e8983608326
Prabu <prabu.mgks@gmail.com>
2022-06-03 10:28:43 +0530

    refactored AboutCard tier list with backerTierList

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,3 +1,4 @@
+import { backerTierList } from "../data/backerTierList";
 import Card from "./Card";
 
 export default function AboutCard() {
@@ -19,90 +20,44 @@ export default function AboutCard() {
         </p>
 
         <div className="grid gap-6">
-          <Card borderColor="border-gray-200" mdPx="md:px-8">
-            <div className="text-left text-sm md:text-base">
-              <div className="md:flex justify-between items-center mb-6">
-                <h3 className="font-bold mb-1 md:m-0 md:text-lg">
-                  Bamboo Stand
-                </h3>
-                <p className="text-moderateCyan font-medium md:text-[0.9375rem]">
-                  Pledge $25 or more
-                </p>
-              </div>
+          {backerTierList.map((backerTier) => {
+            const noStockLeft = backerTier.stockLeft <= 0;
 
-              <p className="text-darkGray leading-6 mb-7 md:leading-8">
-                You get an ergonomic stand made of natural bamboo. You&apos;ve
-                helped us launch our promotional campaign, and you&apos;ll be
-                added to a special Backer member list.
-              </p>
+            return (
+              <Card
+                borderColor="border-gray-200"
+                mdPx="md:px-8"
+                custom={noStockLeft ? "opacity-50" : ""}
+              >
+                <div className="text-left text-sm md:text-base">
+                  <div className="md:flex justify-between items-center mb-6">
+                    <h3 className="font-bold mb-1 md:m-0 md:text-lg">
+                      {backerTier.title}
+                    </h3>
+                    <p className="text-moderateCyan font-medium md:text-[0.9375rem]">
+                      {backerTier.subtitle}
+                    </p>
+                  </div>
 
-              <div className="flex flex-col items-start gap-6 md:flex-row md:items-center md:justify-between">
-                <div className="flex items-center gap-2">
-                  <p className="text-[2rem] font-bold">101</p>
-                  <p className="text-darkGray">left</p>
+                  <p className="text-darkGray leading-6 mb-7 md:leading-8">
+                    {backerTier.description}
+                  </p>
+
+                  <div className="flex flex-col items-start gap-6 md:flex-row md:items-center md:justify-between">
+                    <div className="flex items-center gap-2">
+                      <p className="text-[2rem] font-bold">
+                        {backerTier.stockLeft}
+                      </p>
+                      <p className="text-darkGray">left</p>
+                    </div>
+                    <button className={noStockLeft ? "disabledBtn" : "btn"}>
+                      {noStockLeft ? "Out of stock" : "Select Reward"}
+                    </button>
+                  </div>
                 </div>
-                <button className="btn">Select Reward</button>
-              </div>
-            </div>
-          </Card>
-
-          <Card borderColor="border-gray-200" mdPx="md:px-8">
-            <div className="text-left text-sm md:text-base">
-              <div className="md:flex justify-between items-center mb-6">
-                <h3 className="font-bold mb-1 md:m-0 md:text-lg">
-                  Black Edition Stand
-                </h3>
-                <p className="text-moderateCyan font-medium md:text-[0.9375rem]">
-                  Pledge $75 or more
-                </p>
-              </div>
-
-              <p className="text-darkGray leading-6 mb-7 md:leading-8">
-                You get a Black Special Edition computer stand and a personal
-                thank you. You&apos;ll be added to our Backer member list.
-                Shipping is included.
-              </p>
-
-              <div className="flex flex-col items-start gap-6 md:flex-row md:items-center md:justify-between">
-                <div className="flex items-center gap-2">
-                  <p className="text-[2rem] font-bold">64</p>
-                  <p className="text-darkGray">left</p>
-                </div>
-                <button className="btn">Select Reward</button>
-              </div>
-            </div>
-          </Card>
-
-          <Card
-            borderColor="border-gray-200"
-            mdPx="md:px-8"
-            custom="opacity-50"
-          >
-            <div className="text-left text-sm md:text-base">
-              <div className="md:flex justify-between items-center mb-6">
-                <h3 className="font-bold mb-1 md:m-0 md:text-lg">
-                  Mahogany Special Edition
-                </h3>
-                <p className="text-moderateCyan font-medium md:text-[0.9375rem]">
-                  Pledge $200 or more
-                </p>
-              </div>
-
-              <p className="text-darkGray leading-6 mb-7 md:leading-8">
-                You get two Special Edition Mahogany stands, a Backer T-Shirt,
-                and a personal thank you. You&apos;ll be added to our Backer
-                member list. Shipping is included.
-              </p>
-
-              <div className="flex flex-col items-start gap-6 md:flex-row md:items-center md:justify-between">
-                <div className="flex items-center gap-2">
-                  <p className="text-[2rem] font-bold">0</p>
-                  <p className="text-darkGray">left</p>
-                </div>
-                <button className="disabledBtn">Out of stock</button>
-              </div>
-            </div>
-          </Card>
+              </Card>
+            );
+          })}
         </div>
       </div>
     </Card>
--- src/data/backerTierList.ts
+++ src/data/backerTierList.ts
@@ -9,8 +9,8 @@ export const backerTierList: iBackerTierList[] = [
   {
     title: "Bamboo Stand",
     subtitle: "Pledge $25 or more",
-    description: `You get an ergonomic stand made of natural bamboo. You&apos;ve
-    helped us launch our promotional campaign, and you&apos;ll be
+    description: `You get an ergonomic stand made of natural bamboo. You've
+    helped us launch our promotional campaign, and you'll be
     added to a special Backer member list.`,
     stockLeft: 101,
   },
@@ -18,7 +18,7 @@ export const backerTierList: iBackerTierList[] = [
     title: "Black Edition Stand",
     subtitle: "Pledge $75 or more",
     description: `You get a Black Special Edition computer stand and a personal
-    thank you. You&apos;ll be added to our Backer member list.
+    thank you. You'll be added to our Backer member list.
     Shipping is included.`,
     stockLeft: 64,
   },
@@ -26,7 +26,7 @@ export const backerTierList: iBackerTierList[] = [
     title: "Mahogany Special Edition",
     subtitle: "Pledge $200 or more",
     description: `You get two Special Edition Mahogany stands, a Backer T-Shirt,
-    and a personal thank you. You&apos;ll be added to our Backer
+    and a personal thank you. You'll be added to our Backer
     member list. Shipping is included.`,
     stockLeft: 0,
   },
